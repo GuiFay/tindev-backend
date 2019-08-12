f8de31424d7e6f41a7af146ccfdc184dca0b492d
Guilherme Fay Vergara <guilherme.vergara@mp.intra>
2019-08-12 15:01:09 -0300

    backend completo

--- src/routes.js
+++ src/routes.js
@@ -1,14 +1,18 @@
 const express = require('express');
 const DevController = require('./controllers/DevController');
+const LikeController = require('./controllers/LikeController');
+const DislikeController = require('./controllers/DislikeController');
 
 const routes = express.Router()
 
 // GET, POST, PUT, DELET
 routes.get('/', (req, res) => {
-
     return res.json({ mensagem: `Olá ${req.query.name}` })
 });
 
 routes.post('/devs', DevController.store);
+routes.post('/devs/:devId/likes', LikeController.store);
+routes.post('/devs/:devId/dislikes', DislikeController.store);
+routes.get('/devs/', DevController.index);
 
 module.exports = routes
--- src/server.js
+++ src/server.js
@@ -1,15 +1,19 @@
 const express = require('express');
 const mongoose = require('mongoose')
+const cors = require('cors')
+
 const routes = require('./routes')
 
 const server = express();
 
-mongoose.connect('mongodb+srv://guifay:guifay@tindev-eydck.mongodb.net/test?retryWrites=true&w=majority', {
-    useNewUrlParser: true
-});
+mongoose.connect('mongodb://localhost:27017/tindev?retryWrites=true&w=majority',
+    {
+        useNewUrlParser: true
+    });
 
 
 
+server.use(cors())
 server.use(express.json())
 server.use(routes);
 
